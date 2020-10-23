8aa7f6d12d98352fdaf6095739de9c9761a053a2
Joey Sheehy <joey.sheehy@students.ittralee.ie>
2020-10-23 11:24:44 +0100

    Commit to github

---
+++ labsheet8/exercise1/Bicycle.java
@@ -0,0 +1,46 @@
+package labsheet8.exercise1;
+
+public class Bicycle extends Vehicle {
+    private boolean hasABell;
+    private int gearCount;
+
+    public Bicycle() {
+        setHasABell(false);
+        setGearCount(0);
+    }
+
+    public Bicycle(double pr, double len, double hgt, double wgt, String mft, String mod, boolean bell, int gears) {
+        super(pr,len,hgt,wgt,mft,mod);
+        setHasABell(bell);
+        setGearCount(gears);
+    }
+
+    public void setHasABell(boolean bell) {
+        hasABell=bell;
+    }
+
+    private void setGearCount(int gears) {
+        gearCount=gears;
+    }
+
+    public boolean getHasABell() {
+        return hasABell;
+    }
+
+    private int getGearCount() {
+        return gearCount;
+    }
+
+    public String toString() {
+        String str = super.toString() + "\nHas bell: ";
+
+        if(getHasABell())
+            str+="Yes";
+        else
+            str+="No";
+
+        str+="\nGear Count: " + getGearCount();
+
+        return str;
+    }
+}
